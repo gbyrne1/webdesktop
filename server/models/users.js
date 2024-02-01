7dd00820763d4bd9f4d5f500779fcbc312305992
gbyrne1 <byrneg3@newpaltz.edu>
2024-01-31 23:58:42 -0500

    started model funcs

--- server/models/users.js
+++ server/models/users.js
@@ -1,2 +1,15 @@
 const jwt = require('jsonwebtoken');
 const { connect, ObjectId } = require('./mongo');
+
+async function getAll(page = 1, pageSize = 30) {
+    const col = await collection();
+    const items = await col.find().skip((page-1) * pageSize).limit(pageSize).toArray();
+    const total = await col.countDocuments();
+    return { items, total };
+}
+
+async function getById(id) {
+    const col = await collection();
+    const item = await col.findOne({ _id: new ObjectId(id) });
+    return item;
+}
