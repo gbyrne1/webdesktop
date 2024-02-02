852372c0d612481bc1f7d880f1f6a366ed155d60
gbyrne1 <byrneg3@newpaltz.edu>
2024-02-01 23:58:51 -0500

    updated server functions

--- server/models/users.js
+++ server/models/users.js
@@ -13,3 +13,114 @@ async function getById(id) {
     const item = await col.findOne({ _id: new ObjectId(id) });
     return item;
 }
+
+async function add(item) {
+    const col = await collection();
+
+    const result = await col.insertOne(item);
+
+    item._id = result.insertedId;
+    return item;
+}
+
+async function update(item) {
+
+    console.log(item);
+    const col = await collection();
+    const result = await col.findOneAndUpdate(
+        { _id: new ObjectId(item.id) },
+        { $set: item },
+        { returnDocument: 'after' }
+    );
+
+    return result.value;
+}
+
+async function deleteItem(ide) {
+    const col = await collection();
+    const result = await col.deleteOne({ id: Number(ide) });
+    return result.deletedCount;
+}
+
+async function search(searchTerm, page = 1, pageSize = 30) {
+    const col = await collection();
+    const query = {
+        $or: [
+            { handle: { $regex: searchTerm, $options: 'i' } },
+            { email: { $regex: searchTerm, $options: 'i' } },
+        ]
+    };
+
+    const items = await col.find(query).skip((page - 1) * pageSize).limit(pageSize).toArray();
+    const total = await col.countDocuments(query);
+    return { items, total };
+}
+
+async function seed() {
+    const col = await collection();
+    const result = await col.insertMany(data.users);
+    return result.insertedCount;
+}
+
+async function login(email, password) {
+    const col = await collection();
+    const user = await col.findOne({ email });
+    if (!user) {
+        throw new Error('User not found');
+    }
+    if (user.password !== password) {
+        throw new Error('Invalid password');
+    }
+
+    const cleanUser = { ...user, password: undefined };
+    const token = await generateTokenAsync(cleanUser, '1d');
+
+    return { user: cleanUser, token };
+}
+
+async function oAuthLogin(provider, accessToken) {
+    // validate the access token
+    // if valid, return the user
+    // if not, create a new user
+    // return the user
+}
+
+function generateTokenAsync(user, expiresIn) {
+    return new Promise( (resolve, reject) => {
+        jwt.sign(user, process.env.JWT_SECRET ?? "", { expiresIn }, (err, token) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(token);
+            }
+        });
+    });
+}
+
+function verifyTokenAsync(token) {
+    return new Promise( (resolve, reject) => {
+        jwt.verify(token, process.env.JWT_SECRET ?? "", (err, user) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(user);
+            }
+        });
+    });
+}
+
+
+
+module.exports = {
+    getAll,
+    getById,
+    add,
+    update,
+    deleteItem,
+    search,
+    seed,
+    login,
+    oAuthLogin,
+    generateTokenAsync,
+    verifyTokenAsync,
+};
